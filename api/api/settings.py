e4184439ec12bc1e46d36038cda62820b0db3dad
Kanatbekov <adikanatbekov5@gmail.com>
2021-05-22 13:05:41 +0600

    Api Test

--- api/api/settings.py
+++ api/api/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cars.apps.CarsConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
--- api/cars/views.py
+++ api/cars/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from cars.serializers import *
 from cars.models import Car
 from cars.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class CarDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -19,5 +20,4 @@ class CarListView(generics.ListAPIView):
 
 class CarCreateView(generics.CreateAPIView):
     serializer_class = CarDetailSerializer
-
-
+    permission_classes = (IsAuthenticated, )
